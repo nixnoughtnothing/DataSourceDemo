03487e9645d4704b584f8814c319dcb8bcef415a
nixnoughtnothing <ryoh.t778@gmail.com>
2017-01-27 18:45:06 +0900

    add tabbar vc

---
+++ DatasourceDemo/Controllers/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarControllerViewController.swift
+//  DatasourceDemo
+//
+//  Created by nixnoughtnothing on 1/27/17.
+//  Copyright © 2017 Sttir. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+    // MARK: LifeCycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+}
